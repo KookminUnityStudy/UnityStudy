25b53642bf42098024206647f8375775a66c62ca
Kay <kthss01@naver.com>
2016-06-30 18:00:54 +0900

    c++test

--- Main1016.cpp
+++ Main1016.cpp
@@ -1,37 +1,4 @@
-//#include <iostream>
-//#include <vector>
-//#include <algorithm>
-//#include <functional>
-//
-//using namespace std;
-//
-//long long numList[100001];
-//
-//int main() {
-//
-//	freopen("input.txt", "r", stdin);
-//	int testcase, num;
-//	cin >> testcase;
-//
-//	while ((testcase--) != 0) {
-//		long long sum[100001];
-//
-//		for (int i = 0; i < testcase; i++) {
-//			cin >> num;
-//			numList[i+1] = num;
-//			sum[i] = numList[i - 1] + num;
-//		}
-//
-//		sort(sum, sum + testcase, greater<long long>());
-//
-//		cout << sum[1] << endl;
-//	}
-//
-//	return 0;
-//}
 
-
-/////////////////////////////////////////////
 #include <iostream>
 #include <vector>
 using namespace std;
